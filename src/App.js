408f950c7fba797caabe00d76edd20c3f58569a9
Sergey_Einberg <Sergey_Einberg@epam.com>
2018-01-16 12:23:39 +0600

    Added cdd modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -43,16 +43,9 @@ class App extends Component {
     };
 
     render() {
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        };
 
         let persons = null;
+        let btnClass = '';
 
         if (this.state.showPersons) {
             persons = (
@@ -67,25 +60,24 @@ class App extends Component {
                     })}
                 </div>
             );
-
-            style.backgroundColor = 'red';
+            btnClass = classes.Red;
         }
 
-        let classes = [];
+        let assignedClasses = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red');
+            assignedClasses.push(classes.red);
         }
         if (this.state.persons.length <= 1) {
-            classes.push('bold');
+            assignedClasses.push(classes.bold);
         }
 
 
         return (
-                <div className="App">
+                <div className={classes.App}>
                     <h1>Hi, I'm a React App</h1>
-                    <p className={classes.join(' ')}>This is realy working!</p>
+                    <p className={assignedClasses.join(' ')}>This is realy working!</p>
                     <button
-                        style={style}
+                        className={btnClass}
                         onClick={this.togglePersonHandler}>Toggle Persons
                     </button>
                     {persons}
