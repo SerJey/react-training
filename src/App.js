f62eb1fd590e08a1411f4c8a28cfa768a826cd4c
Sergey_Einberg <Sergey_Einberg@epam.com>
2018-01-16 11:31:33 +0600

    Added dynamic styles

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ class App extends Component {
 
     render() {
         const style = {
-            backgroundColor: 'white',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
@@ -66,17 +67,29 @@ class App extends Component {
                     })}
                 </div>
             );
+
+            style.backgroundColor = 'red';
         }
 
+        let classes = [];
+        if (this.state.persons.length <= 2) {
+            classes.push('red');
+        }
+        if (this.state.persons.length <= 1) {
+            classes.push('bold');
+        }
+
+
         return (
-            <div className="App">
-                <h1>Hi, I'm a React App</h1>
-                <button
-                    style={style}
-                    onClick={this.togglePersonHandler}>Toggle Persons
-                </button>
-                {persons}
-            </div>
+                <div className="App">
+                    <h1>Hi, I'm a React App</h1>
+                    <p className={classes.join(' ')}>This is realy working!</p>
+                    <button
+                        style={style}
+                        onClick={this.togglePersonHandler}>Toggle Persons
+                    </button>
+                    {persons}
+                </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
     }
